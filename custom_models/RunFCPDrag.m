500a49c1221544d98bfeb20306b492bd1a0174ad
Eric Bokenfohr <edbokenfohr@gmail.com>
2023-02-16 13:39:49 -0800

    Started writing code to run custom sims.

---
+++ custom_models/RunFCPDrag.m
@@ -0,0 +1,16 @@
+customModel = CustomModel('FCPDrag');
+chassis = chassis_instance_FCP_SEMA2022;
+aero = aero_assump_2006Prototype;
+customModel.components = {chassis ; aero};
+
+user = CustomUser();
+user.startTime = 0;
+user.stopTime = 7;
+user.timeProfile = [user.startTime : 0.1 : user.stopTime]';
+
+user.inputs.vehicleSpeed = user.timeProfile * 26.6 / 3.6;
+
+assump = AssumptionSet(customModel, user);
+
+customSim = Simulation(assump);
+res = customSim.run();
